baa004b1d1accc288ddf39c6c1f20adb53d011dc
Mike Graves <mgraves@mit.edu>
2013-11-27 08:37:12 -0500

    Remove hardcoded path to XSL

--- kendallite/application/settings.py
+++ kendallite/application/settings.py
@@ -1,7 +1,10 @@
+import os
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
 
 SHIB_LOGIN_URL = 'https://delisle-r1.mit.edu/Shibboleth.sso/Login'
 SOLR_URL = 'http://localhost/solr/'
 STORAGE_ENGINE = 'kendallite.core.storage.solr'
-FGDC_XSL = '/home/gravm/git/kendallite/kendallite/application/fgdc.xsl'
+FGDC_XSL = os.path.join(current_dir, 'fgdc.xsl')
 STATIC_URL = '/static/'
 SITE_ROOT = ''
