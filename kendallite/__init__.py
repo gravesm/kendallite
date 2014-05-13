a7673ebb683bae7c798a98c16a5a6bfb66d2406f
Mike Graves <mgraves@mit.edu>
2014-05-13 11:51:38 -0400

    Add logging setup

--- kendallite/__init__.py
+++ kendallite/__init__.py
@@ -6,4 +6,9 @@ os.environ.setdefault("KENDALLITE_SETTINGS", "site/settings.py")
 app = Flask(__name__)
 app.config.from_envvar('KENDALLITE_SETTINGS')
 
+# I cannot get the LOGGER_NAME config option, which seems like the right way to
+# do this, to work.
+for handler in app.config.get('HANDLERS', []):
+    app.logger.addHandler(handler)
+
 import kendallite.site.views
